14c09a862d9f665ce4785345fca6fe2b811c411e
Corentin Godefroy <48216051+corentin-godefroy@users.noreply.github.com>
2024-10-25 11:44:56 +0200

    Update test_conteneurcreator.py

--- test_conteneurcreator.py
+++ test_conteneurcreator.py
@@ -1,9 +1,1 @@
- import pytest
- import platform
- from conteneurcreator import is_docker_installed
- def test_system_detection():
-    system = platform.system()
-    assert system in ["Linux", "Windows"]
- def test_docker_installed():
-    result = is_docker_installed()
-    assert isinstance(result, bool)
+
